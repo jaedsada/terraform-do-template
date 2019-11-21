e4cc485cc55acfaa5df84611c373f3865dd91eb5
thnovice <jaedsada@issoft.co.th>
2019-11-21 22:23:16 +0700

    first commit

---
+++ Variables.tf
@@ -0,0 +1,8 @@
+variable "do_token" {}
+variable "pub_key" {}
+variable "pvt_key" {}
+variable "ssh_fingerprint" {}
+variable "droplet_name" {}
+variable "droplet_size" {}
+variable "droplet_image" {}
+variable "droplet_region" {}
